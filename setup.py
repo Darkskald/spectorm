8fa917048e7966b1e735f2b1ca550463bd2b3af1
darkskald <floriandavid.lange@web.de>
2019-12-02 20:58:38 +0100

    test

--- setup.py
+++ setup.py
@@ -1,3 +1,21 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-# setup()
+with open("README.md", "r") as infile:
+    long_description = infile.read()
+
+setup(
+    name="spectorm",
+    version="0.0.1",
+    author="Florian-David Lange",
+    author_email="lange@phc.uni-kielde",
+    description="An extensible toolset for spectral data handling",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Darkskald/spectorm",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
---
+++ spectorm/spectrum/spectrum_meta.py
@@ -0,0 +1,4 @@
+class Test:
+
+    def test(self):
+        print("Spectrum package calling!")
